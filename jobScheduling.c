b0ea4505f88fa82817af3200773f0c6de837be93
Md Anees Khan <khanmdanis145@gmail.com>
2025-10-17 12:27:09 +0530

    Refactor job scheduling algorithm
    Refactor job scheduling algorithm to improve readability and efficiency. Updated job input and sorting logic.

--- jobScheduling.c
+++ jobScheduling.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
-#define MAX 10
 
 struct Job
 {
@@ -8,94 +6,71 @@ struct Job
     int deadline;
     int profit;
 };
-
-void sortJobs(struct Job jobs[], int n)
+int main(int argc, char const *argv[])
 {
-    int i, j;
-    struct Job temp;
-    for (i = 0; i < n - 1; i++)
+    int n, maxDeadline = 0, maxProfit = 0;
+    printf("Enter the number of jobs: ");
+    scanf("%d", &n);
+    Job jobs[n];
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < n - i - 1; j++)
+        printf("Enter the deadline and profit of Job-%d: ", (i + 1));
+        jobs[i].id = i + 1;
+        scanf("%d %d", &jobs[i].deadline, &jobs[i].profit);
+    }
+    // sort jobs according to max profit
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
         {
-            if (jobs[j].profit < jobs[j + 1].profit)
+            if (jobs[j].profit > jobs[j + 1].profit)
             {
-                temp = jobs[j];
+                Job temp = jobs[j];
                 jobs[j] = jobs[j + 1];
                 jobs[j + 1] = temp;
             }
         }
     }
-}
-
-void jobSequencing(struct Job jobs[], int n)
-{
-    int i, k;
-    int maxDeadline = 0;
-    int timeslot[MAX] = {0}; // Initialize all elements to 0
-    int totalProfit = 0;
-
-    // Find the maximum deadline
-    for (i = 0; i < n; i++)
+    // claculate max deadline
+    for (int i = 0; i < n; i++)
     {
         if (jobs[i].deadline > maxDeadline)
             maxDeadline = jobs[i].deadline;
     }
+    // making a slot for jobs and an array to show job sequences
+    int timeSlot[maxDeadline + 1] = {0};
 
-    // Ensure maxDeadline does not exceed MAX
-    if (maxDeadline >= MAX)
+    for (int i = 0; i < n; ++i)
     {
-        maxDeadline = MAX - 1;
-    }
-
-    // Initialize timeslot array
-    for (i = 1; i <= maxDeadline; i++)
-    {
-        timeslot[i] = -1;
-    }
-
-    // Assign jobs to timeslots
-    for (i = 0; i < n; i++)
-    {
-        k = (jobs[i].deadline <= maxDeadline) ? jobs[i].deadline : maxDeadline;
-        while (k >= 1)
+        if (timeSlot[jobs[i].deadline] == 0)
         {
-            if (timeslot[k] == -1)
+            maxProfit += jobs[i].profit;
+            timeSlot[jobs[i].deadline] = jobs[i].id;
+        }
+        else
+        {
+            int j = jobs[i].deadline;
+            while (j > 0)
             {
-                timeslot[k] = i;
-                totalProfit += jobs[i].profit;
-                break;
+                if (timeSlot[j] == 0)
+                {
+                    maxProfit += jobs[i].profit;
+                    timeSlot[j] = jobs[i].id;
+                    break;
+                }
+                else
+                {
+                    j--;
+                }
             }
-            k--;
         }
     }
-
-    // Print job sequence
-    printf("\nJob sequence (with Deadlines): \n");
-    for (i = 1; i <= maxDeadline; i++)
+    printf("\nMax profit = %d\n", maxProfit);
+    printf("Job Sequences:\n");
+    for (int i = 1; i <= maxDeadline; ++i)
     {
-        if (timeslot[i] != -1)
-            printf("slot %d -> Job%d (profit = %d)\n", i, jobs[timeslot[i]].id, jobs[timeslot[i]].profit);
-        else
-            printf("slot %d -> Empty\n", i);
+        printf("%d-->%d : J%d \n", i - 1, i, timeSlot[i]);
     }
-    printf("\nTotal Profit = %d\n", totalProfit);
-}
-
-int main()
-{
-    struct Job jobs[MAX];
-    int n, i; // Declare i here
-    printf("Enter number of jobs: ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        printf("\nEnter details for Job %d: \n", i + 1);
-        jobs[i].id = i + 1;
-        scanf("%d", &jobs[i].deadline);
-        scanf("%d", &jobs[i].profit);
-    }
-    sortJobs(jobs, n);
-    jobSequencing(jobs, n);
-    getch();
-    return 0; // Return 0 to indicate successful execution
+    printf("\n");
+    return 0;
 }
