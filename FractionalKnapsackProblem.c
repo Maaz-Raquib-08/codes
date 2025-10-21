807269fd4f30227822fc928214525df84bf425b3
Md Anees Khan <khanmdanis145@gmail.com>
2025-10-21 16:37:21 +0530

    Update FractionalKnapsackProblem.c

--- FractionalKnapsackProblem.c
+++ FractionalKnapsackProblem.c
@@ -1,95 +1,67 @@
 #include <stdio.h>
 
-// --- Merge function ---
-void merge(float ratio[], float weight[], float profit[], int low, int mid, int high) {
-    int i = low, j = mid + 1, k = 0;
-    float tr[high - low + 1], tw[high - low + 1], tp[high - low + 1];
+// --- Bubble Sort function (descending order by ratio) ---
+void bubbleSort(float ratio[], float weight[], float profit[], int n) {
+    int i, j;
+    float temp;
 
-    // Merge two sorted halves (descending order by ratio)
-    while (i <= mid && j <= high) {
-        if (ratio[i] >= ratio[j]) {
-            tr[k] = ratio[i];
-            tw[k] = weight[i];
-            tp[k] = profit[i];
-            i++;
-        } else {
-            tr[k] = ratio[j];
-            tw[k] = weight[j];
-            tp[k] = profit[j];
-            j++;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (ratio[j] < ratio[j + 1]) {
+                // swap ratio
+                temp = ratio[j];
+                ratio[j] = ratio[j + 1];
+                ratio[j + 1] = temp;
+
+                // swap weight
+                temp = weight[j];
+                weight[j] = weight[j + 1];
+                weight[j + 1] = temp;
+
+                // swap profit
+                temp = profit[j];
+                profit[j] = profit[j + 1];
+                profit[j + 1] = temp;
+            }
         }
-        k++;
-    }
-
-    while (i <= mid) {
-        tr[k] = ratio[i];
-        tw[k] = weight[i];
-        tp[k] = profit[i];
-        i++; k++;
-    }
-    while (j <= high) {
-        tr[k] = ratio[j];
-        tw[k] = weight[j];
-        tp[k] = profit[j];
-        j++; k++;
-    }
-
-    // Copy back to original arrays
-    for (i = low, k = 0; i <= high; i++, k++) {
-        ratio[i] = tr[k];
-        weight[i] = tw[k];
-        profit[i] = tp[k];
-    }
-}
-
-// --- Merge Sort recursive function ---
-void mergeSort(float ratio[], float weight[], float profit[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        mergeSort(ratio, weight, profit, low, mid);
-        mergeSort(ratio, weight, profit, mid + 1, high);
-        merge(ratio, weight, profit, low, mid, high);
     }
 }
 
 // --- Knapsack function ---
 void knapsack(int n, float weight[], float profit[], float capacity) {
     float x[20], tp = 0;
-    int i, u;
-    u = capacity;
+    int i;
+    float u = capacity;
 
     for (i = 0; i < n; i++) {
         x[i] = 0.0;
     }
 
     for (i = 0; i < n; i++) {
-        if (weight[i] > u) {
+        if (weight[i] > u)
             break;
-        } else {
+        else {
             x[i] = 1.0;
             tp = tp + profit[i];
             u = u - weight[i];
         }
     }
 
-    if (i < n) {
-        x[i] = u / weight[i];
-        tp = tp + (x[i] * profit[i]);
-    }
+    if (i < n)
+        x[i] = u / weight[i], tp = tp + (x[i] * profit[i]);
 
-    printf("\nMaximum profit is: %f", tp);
+    printf("\nMaximum profit is: %.2f\n", tp);
 }
 
 // --- Main function ---
 int main() {
-    float weight[20], profit[20], capacity;
+    float weight[20], profit[20], capacity, ratio[20];
     int num, i;
-    float ratio[20];
 
-    printf("\nEnter the no. of objects: ");
+    printf("\nEnter the number of objects: ");
     scanf("%d", &num);
 
-    printf("\nEnter the weights and profits of each object: ");
+    printf("\nEnter the weights and profits of each object:\n");
     for (i = 0; i < num; i++) {
         scanf("%f %f", &weight[i], &profit[i]);
     }
@@ -101,8 +73,8 @@ int main() {
         ratio[i] = profit[i] / weight[i];
     }
 
-    // --- Sort using Merge Sort ---
-    mergeSort(ratio, weight, profit, 0, num - 1);
+    // --- Sort using Bubble Sort ---
+    bubbleSort(ratio, weight, profit, num);
 
     // --- Call Knapsack ---
     knapsack(num, weight, profit, capacity);
